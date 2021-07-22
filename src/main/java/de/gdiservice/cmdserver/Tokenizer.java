4cb68d737c87ead9b7b13e1121f7f8d7fec0250a
rtrier <ralf.trier@gdi-service.de>
2021-07-22 13:11:16 +0200

    escapeing backslash

--- src/main/java/de/gdiservice/cmdserver/Tokenizer.java
+++ src/main/java/de/gdiservice/cmdserver/Tokenizer.java
@@ -16,7 +16,7 @@ public class Tokenizer {
         
         for (int i=0, count=s.length(); i<count; i++) {            
             char ch = s.charAt(i);
-            if (ch == '\\' && i<count-1 && s.charAt(i+1)=='"') {
+            if (ch == '\\' && i<count-1 && (s.charAt(i+1)=='"' || s.charAt(i+1)=='\\')) {
                 sb.append(s.charAt(i+1));
                 i++;
             } else {
@@ -52,7 +52,7 @@ public class Tokenizer {
     
     public static void main(String[] args) {
         // String s = "-f \"PostgreSQL\" PG:\"host='pgsql' port='5432' dbname='kvwmapsp' user='kvwmap' ACTIVE_SCHEMA=testschema_ralf \" GMLAS:/var/www/tmp/temp.gml_2.gml -oo REMOVE_UNUSED_LAYERS=YES -oo XSD=/var/www/html/modell/xsd/5.1/XPlanung-Operationen.xsd -lco SCHEMA=testschema_ralf";
-        String s = "-lco FID=gid -f PostgreSQL -lco GEOMETRY_NAME=the_geom -lco precision=NO -nlt PROMOTE_TO_MULTI -nln afrei_1polygon908580 -a_srs EPSG:2398 -append PG:\"host='pgsql' port='5432' dbname='kvwmapsp' user='k\\vwmap' password='-1$ß~bN\\\"6&ZWPe}B_YpA+l' active_schema=custom_shapes\" \"/var/www/data/upload/11/Frei 1Polygon.dbf\"";
+        String s = "-lco FID=gid -f PostgreSQL -lco GEOMETRY_NAME=the_geom -lco precision=NO -nlt PROMOTE_TO_MULTI -nln afrei_1polygon908580 -a_srs EPSG:2398 -append PG:\"host='pgsql' port='5432' dbname='kvwmapsp' user='k\\\\\\\"vwmap' password='-1$ß~bN\\\"6&ZWPe}B_YpA+l' active_schema=custom_shapes\" \"/var/www/data/upload/11/Frei 1Polygon.dbf\"";
         // String s = "-f \"PostgreSQL\" PG:\"host='pgsql'";
         String[] tokens = getTokens(s);
         for (int i=0; i<tokens.length; i++) {
